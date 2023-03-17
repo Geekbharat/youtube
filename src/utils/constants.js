8bb69de2a77cbcad564d0275aab3a755e9b7da2f
Geekbharat <patrabharat36@gmail.com>
2023-03-17 20:19:14 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 const GOOGLE_API_KEY = "AIzaSyDh7a_V8dRfe5gPlhR65FNlxl2t0EZFWn0"
 
 export const OFFSET_LIVE_CHAT = 20;
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API = ""+ GOOGLE_API_KEY;
 
 
 
